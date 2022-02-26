ecf8af536def9a20ad42bbe15232c1800cf7e352
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-02-26 23:48:51 +0600

    api calls

--- food-api/script.js
+++ food-api/script.js
@@ -1,4 +1,18 @@
 
-document.getElementById('search-btn').addEventListener('click', function () {
-    console.log('Button clicked');
-})
+// document.getElementById('search-btn').addEventListener('click', function () {
+//     console.log('Button clicked');
+// })
+
+const searchFood = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    searchField.value = '';
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data.meals));
+
+}
