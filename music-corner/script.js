b7d7b70421b0a32aac2aeb78709553d95dd1fb9f
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-03-10 16:19:37 +0600

    misuc corner template updated

--- music-corner/script.js
+++ music-corner/script.js
@@ -1,9 +1,13 @@
 const elementById = (id) => {
-  document.getElementById(id);
+  return document.getElementById(id);
 };
-
+const artistContainer = elementById("artists");
+const albumContainer = elementById("albums");
 const handleSearch = () => {
+  artistContainer.innerHTML = "";
+  albumContainer.innerHTML = "";
   const keyword = elementById("keyword");
+  // console.log(keyword);
   const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
   fetch(url)
     .then((res) => res.json())
@@ -11,14 +15,19 @@ const handleSearch = () => {
 };
 
 const showArtists = (data) => {
-  const artistContainer = elementById("artist");
+  artistContainer.innerHTML = "";
+  // console.log(artistContainer);
   data?.artists?.forEach((artist) => {
     const div = document.createElement("div");
     div.classList.add("artist-card");
     div.innerHTML = `<div class="image-container">
     <div class="image-container-inner">
       <img
-        src="${artist.strArtistThumb}"
+        src="${
+          artist.strArtistThumb
+            ? artist.strArtistThumb
+            : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFpuWD5ahdQQzZDNi5PNqPcp1P_oCr6r4p0A&usqp=CAU"
+        }"
         alt=""
       />
     </div>
@@ -30,35 +39,46 @@ const showArtists = (data) => {
   </div>
   <button class="album-button">
     <i class="fa-solid fa-compact-disc"></i>
-    <p onclick="fetchAlbums('${artist.idArtist}')" class="button-title">Albums</p>
+    <p onclick="fetchAlbums('${
+      artist.idArtist
+    }')" class="button-title">Albums</p>
   </button>`;
     artistContainer.appendChild(div);
   });
 };
 
 const fetchAlbums = (id) => {
-  const url = `theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+  const url = `https://theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+  // console.log(url);
   fetch(url)
-    .then((res) => res.JSON())
+    .then((res) => res.json())
     .then((data) => showAlbum(data));
   const artistContainer = elementById("artists");
   artistContainer.innerHTML = "";
 };
 
 const showAlbum = (data) => {
-  const albumContainer = elementById("albums");
-  album.forEach((item) => {
+  // console.log(data);
+
+  albumContainer.innerHTML = "";
+  data.album.forEach((item) => {
+    /*     console.log(data.album);
+    console.log(item); */
     const div = document.createElement("div");
     div.classList.add("album");
     div.innerHTML = `
         <div class="album-image-container">
           <img
-            src="${album.strAlbumThumb}"
+            src="${
+              item.strAlbumThumb
+                ? item.strAlbumThumb
+                : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVQI56utJ6fav7bXDqhE-AK5RbAv4xhp8dNQ&usqp=CAUhttps://image.shutterstock.com/image-illustration/retro-futuristic-background-1980s-style-260nw-1119762995.jpg"
+            }"
             alt=""
           />
         </div>
         <div class="album-name">
-          <h3>${album.strAlbum}</h3>
+          <h3>${item.strAlbum}</h3>
         </div>
       `;
 
